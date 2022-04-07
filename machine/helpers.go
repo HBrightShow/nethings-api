1c06e540c62b3275ca5626da12c9e6719eedf6e3
HBrightShow <angusbright@163.com>
2022-04-07 13:19:21 +0000

    add net

--- machine/helpers.go
+++ machine/helpers.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
 
-	"github.com/nethings/nethings-api/machine/types"
+	"github.com/nethings-api/machine/types"
 )
 
 func getTimeLeft(state types.ProposalState, createTime, warmUpDuration, activeDuration, queueDuration, gracePeriodDuration int64) *int64 {
